874936ecc2eaef862f21c853d29790b8e8911d46
CallumDougan <practicalitymalfunction@gmail.com>
2016-01-11 11:18:47 +0000

    basic CanCan permissions in Ability, ApplicationController

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,6 +2,13 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
+    if user.role? :admin
+      can :manage, :all
+  elsif user.role? :user
+      can :read, :all
+  end
+end
+
     # Define abilities for the passed in user here. For example:
     #
     #   user ||= User.new # guest user (not logged in)
@@ -28,5 +35,5 @@ class Ability
     #
     # See the wiki for details:
     # https://github.com/CanCanCommunity/cancancan/wiki/Defining-Abilities
-  end
 end
+
