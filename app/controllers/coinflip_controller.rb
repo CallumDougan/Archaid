9d1ebbe234ccb1d4dc3d8e575fba4442efd743a3
CallumDougan <practicalitymalfunction@gmail.com>
2016-01-11 09:29:55 +0000

    generated controllers

---
+++ app/controllers/coinflip_controller.rb
@@ -0,0 +1,2 @@
+class CoinflipController < ApplicationController
+end
---
+++ test/controllers/arcade_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class ArcadeControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
