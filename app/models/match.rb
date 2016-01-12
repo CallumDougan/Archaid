0f92f1255059d95f130ce3d9f2ef5c01c72e3459
CallumDougan <practicalitymalfunction@gmail.com>
2016-01-12 15:40:28 +0000

    added move associations

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,6 +1,7 @@
 class Match < ActiveRecord::Base
   has_one :user, foreign_key: "user_1_id", class_name: "User"
   has_one :other_user, foreign_key: "user_2_id", class_name: "User"
+  has_many :moves
   belongs_to :arcade
 
   def player_finder(user)
