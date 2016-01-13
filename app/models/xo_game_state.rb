2bbd5edf647a69a12abd388b258e500ca1a404b0
CallumDougan <practicalitymalfunction@gmail.com>
2016-01-13 15:59:08 +0000

    aaaaaaaaah

--- app/models/xo_game_state.rb
+++ app/models/xo_game_state.rb
@@ -11,131 +11,94 @@ class XoGameState
     @pieces = [:o, :x]
   end
 
+  def show_board
+    @board
+  end
+
+  def next_piece
+    @pieces[ number_of_turns % 2 ]
+  end
+
+  def last_piece
+    @pieces[ (number_of_turns + 1) % 2 ]
+  end
+
+  def drawn?
+    number_of_turns == 9
+  end
+
+  def create_move(row, column)
+    @match.moves.create(row: row, column: column, piece: next_piece)
+    refresh_board()
+    if has_won?(last_piece.to_s)
+      declare_result('win')
+    elsif drawn?
+      declare_result('draw')
+    end
+  end 
+
+  def refresh_board
+    moves = @match.moves
+    moves.each do |move|
+      @board[move.row][move.column] = move.piece if move.row && move.column
+    end
+  end
+
   def number_of_turns
     @match.moves.count
   end
 
-  def draw_check
-   number_of_turns == 9
- end
-
- def horizontal_win
-  @board.each do |row|
-    row.all? do |place|
-      if place == @pieces[0]
-        winner = @pieces[0]
-      elsif place = @pieces[1]
-        winner = @pieces[1]
-      end
+  def has_won?(symbol)
+    if horizontal_line?(symbol, @board) ||
+    vertical_line?(symbol) ||
+    diagonal_line?(symbol)
+      return true
     end
   end
-  winner
-end
 
-
-def horizontal_win2
-  @match.moves.each do |row|
-    row.all? do |place|
-      if place == @match.moves(:row)
-        winner = @match.moves(:row)
-      end
-      winner
+  def horizontal_line?(symbol, board)
+    board.any? do |row|
+      row_has_winning_line(row, symbol)
     end
   end
-end
 
-def horizontal_win3
-  @match.moves.each do |row|
-    row.each do |place|
-      if @match.moves.where(row: place.row) == 3
-        winner = @match.moves(:row)
-      end
-      winner
+  def vertical_line?(symbol)
+    transp_board = @board.transpose
+    horizontal_line?(symbol, transp_board)
+  end
+
+  def diagonal_line?(symbol)
+    middle_piece = @board[1][1]
+    return false if middle_piece != symbol
+    topleft_bottomright = @board[0][0] == symbol && @board[2][2] == symbol
+    bottomleft_topright = @board[2][0] == symbol && @board[0][2] == symbol
+    topleft_bottomright || bottomleft_topright
+  end
+
+  def row_has_winning_line(row, symbol)
+    row.all? do |square|
+    square == symbol
     end
   end
-end
 
-def straight_line_win
-#check to see if any row or column is filled, checks if the piece is the same in all three places, returns winning piece
-row_zero = @match.moves.where(row: 0)
-row_one = @match.moves.where(row: 1)
-row_two = @match.moves.where(row: 2)
-column_zero = @match.moves.where(column: 0)
-column_one = @match.moves.where(column: 1)
-column_two = @match.moves.where(column: 2)
-line_wins = [row_zero, row_one, row_two, column_zero, column_one, column_two]
-line_wins.find do |line|
-  if line.count == 3
-    winner = line(:row)
-  else
-    winner = nil
-  end
-  winner
-end
-end
 
-def vertical_win
-  @board.transpose.each do |column|
-    column.all? do |place|
-      if place == @pieces[0]
-        winner = @pieces[0]
-      elsif place == @pieces[1]
-        winner = @pieces[1]
+
+  def declare_result(result)
+    #updates @match.winner to reflect result (0 for draw), redirect to custom_route?
+    if result == 'draw'
+      @match.winner = 0
+    else
+      if number_of_turns.odd?
+        @match.winner = @match.user_1_id
+      else
+        @match.winner = @match.user_2_id
       end
+      # "This game was won by #{@match.winner(:name)}"
     end
+
+    @match.game_state = "completed"
+    @match.save
+    # redirect_to user_path
   end
-  winner
-end
-
-def diagonal_win
-  middle_place = @board[1][1]
-  if middle_place !nil && middle_place == @board[0][0] && middle_place == @board [2][2]
-    winner = middle_place
-  elsif middle_place ! nil && middle_place == @board[2][0] && middle_place == @board[0][2]
-    winner = middle_place
-  end
-  winner
-end
-
-def win_check
-  if horizontal_win?
-    return true
-  elsif vertical_win?
-    return true
-  elsif diagonal_win?
-    return true
-  else
-    return false
-  end
-end
-
-def end_check
-  if draw_check?
-#note result, redirect_to custom route?
-elsif win_check?
-#note result, redirect_to custom route?
-end
-end
-
-def declare_winner
-  #updates match info to reflect winner
-end
-
-def show_board
-  @board
-end
-
-def create_move(row, column)
-  piece = @pieces[ number_of_turns % 2 ]
-  @match.moves.create(row: row, column: column, piece: piece)
-  end_check
-end 
-
-def refresh_board
-  moves = @match.moves
-  moves.each do |move|
-    @board[move.row][move.column] = move.piece if move.row && move.column
-  end
-end
 
 end
