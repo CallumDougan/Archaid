b66b7336db109e4bef417c896a4027e12d3c47cb
CallumDougan <practicalitymalfunction@gmail.com>
2016-01-13 18:14:15 +0000

    added win record to user profiles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -34,6 +34,8 @@ class UsersController < ApplicationController
 
     end
 
+    
+
 
 
     private
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-         has_many :matches
+  :recoverable, :rememberable, :trackable, :validatable
+  has_many :matches
 
   before_save :default_values
 
@@ -18,6 +18,13 @@ class User < ActiveRecord::Base
   def user_image
     @image
   end
+
+  def win_record
+  wins = Match.where(winner: self.id).count
+  # binding.pry
+  percentage = wins.fdiv(Match.all.count) * 100
+
+ end
 
 
 end
